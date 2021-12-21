70c1e337740626099287dea812089497bb6a583a
David Miller <15317284+pcktbot@users.noreply.github.com>
2021-12-21 13:51:56 -0800

    static config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,11 @@
 module.exports ={
-  base: '/opex-runbook/'
+  base: '/opex-runbook/',
+  themeConfig: {
+    logo: '/midnight-g5-logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Postgres', link: '/postgres/' },
+      { text: 'Contributing', link: '/contributing/' }
+    ]
+  }
 }
