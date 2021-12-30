bdd6e1b1e29218155d8e755d3ca51acb220293fd
Johnny Archer <johnny.archer@getg5.com>
2021-12-30 12:00:41 -0800

    Resolve Deployment Issues

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 npm run docs:build
 cd docs/.vuepress/dist
-git init
+git init --initial-branch=main
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:g5search/opex-runbook.git main:gh-pages
